a4455203575d6f379bcad615b359adfd6bf712a7
Andreas Link <mail@linkandreas.de>
2025-01-19 19:30:13 +0100

    Add existing data from previous rounds

---
+++ src/pages/existing_data.tsx
@@ -0,0 +1,54 @@
+export default function preview_data() {
+  return {
+    participants: [
+      "Alisa C.",
+      "Andreas B.",
+      "Andreas H.",
+      "Dieter A.",
+      "Günter B.",
+      "Jörg W.",
+      "Karin M.",
+      "Marc H.",
+      "Michael H.",
+      "Michael d.",
+      "Ralf D.",
+      "Torsten K.",
+      "Laura B.",
+      "Julian M.",
+    ],
+    previousRounds: [
+      [
+        ["Alisa C.", "Andreas B."],
+        ["Karin M.", "Marc H."],
+        ["Andreas H.", "Jörg W."],
+        ["Dieter A.", "Michael d."],
+        ["Ralf D.", "Günter B."],
+        ["Torsten K.", "Michael H."],
+      ],
+      [
+        ["Alisa C.", "Marc H."],
+        ["Torsten K.", "Dieter A."],
+        ["Andreas B.", "Günter B."],
+        ["Michael H.", "Karin M."],
+        ["Jörg W.", "Ralf D."],
+        ["Andreas H.", "Michael d."],
+      ],
+      [
+        ["Alisa C.", "Dieter A."],
+        ["Michael H.", "Michael d."],
+        ["Torsten K.", "Karin M."],
+        ["Andreas H.", "Ralf D."],
+        ["Marc H.", "Günter B."],
+        ["Jörg W.", "Andreas B."],
+      ],
+      [
+        ["Jörg W.", "Dieter A."],
+        ["Ralf D.", "Marc H."],
+        ["Karin M.", "Andreas B."],
+        ["Michael H.", "Alisa C."],
+        ["Torsten K.", "Andreas H."],
+        ["Günter B.", "Michael d."],
+      ],
+    ],
+  };
+}
