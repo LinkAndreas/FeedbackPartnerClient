9b413524fc1a79c81abd4a69e1e59c30d84dea69
Andreas Link <mail@linkandreas.de>
2025-01-19 19:41:39 +0100

    Support Dark Mode

--- src/pages/overview.tsx
+++ src/pages/overview.tsx
@@ -56,16 +56,16 @@ export default function Overview() {
   }
 
   return (
-    <div className="min-h-screen flex items-center justify-center p-4">
+    <div className="min-h-screen flex items-center justify-center p-4 bg-white dark:bg-gray-900">
       <div className="w-full max-w-4xl">
-        <h1 className="text-2xl font-bold text-center mb-6">Feedback-Paar-Generator</h1>
+        <h1 className="text-2xl font-bold text-center mb-6 text-black dark:text-white">Feedback-Paar-Generator</h1>
 
         {/* Teilnehmer */}
         <div>
-          <h2 className="text-lg font-semibold mb-2">Teilnehmer:</h2>
-          <p>Namen aller Teilnehmer (ein Name je Zeile)</p>
+          <h2 className="text-lg font-semibold mb-2 text-black dark:text-white">Teilnehmer:</h2>
+          <p className="my-4 text-black dark:text-white">Namen aller Teilnehmer (ein Name je Zeile)</p>
           <textarea
-            className="w-full p-2 border rounded"
+            className="w-full p-2 border rounded bg-white dark:bg-gray-800 text-black dark:text-white border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-2 focus:ring-teal-500"
             rows={6}
             placeholder={`Name_1\nName_2\nName_3\nName_4`}
             value={participants}
@@ -76,14 +76,14 @@ export default function Overview() {
         {/* Feedback-Partner */}
         {rounds.length > 0 && (
           <div>
-            <h2 className="text-lg font-semibold mb-2">Feedback Paare je Runde:</h2>
+            <h2 className="text-lg font-semibold mb-2 text-black dark:text-white">Feedback Paare je Runde:</h2>
             {rounds.map((round, index) => (
               <div key={index} className="mb-2">
-                <h3 className="text-md font-semibold">Runde {index + 1}</h3>
-                <p>Feedback Paare aus Runde {index + 1} der Form 'Name1, Name2' (ein Paar je Zeile)</p>
+                <h3 className="text-md font-semibold text-black dark:text-white">Runde {index + 1}</h3>
+                <p className="my-4 text-black dark:text-white">Feedback Paare aus Runde {index + 1} der Form 'Name1, Name2' (ein Paar je Zeile)</p>
                 <div className="flex items-start gap-2">
                   <textarea
-                    className="w-full p-2 border rounded"
+                    className="w-full p-2 border rounded bg-white dark:bg-gray-800 text-black dark:text-white border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-2 focus:ring-teal-500"
                     rows={7}
                     placeholder={'Name_1, Name_4\nName_2, Name_3'}
                     value={round}
@@ -100,9 +100,9 @@ export default function Overview() {
             ))}
           </div>)}
 
-        { /* Aktionen */}
+        {/* Aktionen */}
         <div className="flex flex-col content-center mb-4">
-          <h2 className="text-lg font-semibold mb-2">Aktionen:</h2>
+          <h2 className="text-lg font-semibold mb-2 text-black dark:text-white">Aktionen:</h2>
           {/* Feedback-Paare anlegen */}
           <button
             className="mb-4 bg-teal-500 text-white px-4 py-2 rounded hover:bg-teal-600"
@@ -121,8 +121,8 @@ export default function Overview() {
 
         {/* Bestehende Daten */}
         <div className="mb-4">
-          <h2 className="text-lg font-semibold mb-2">Bestehende Daten:</h2>
-          <p className="mb-4">Nachfolgend können die bestehenden Feedback-Paare aus Runde 1-4 geladen werden:</p>
+          <h2 className="text-lg font-semibold mb-2 text-black dark:text-white">Bestehende Daten:</h2>
+          <p className="mb-4 text-black dark:text-white">Nachfolgend können die bestehenden Feedback-Paare aus Runde 1-4 geladen werden:</p>
           <div className="mb-4 text-center">
             <button
               className="w-full bg-amber-500 text-white px-4 py-2 rounded hover:bg-amber-600"
