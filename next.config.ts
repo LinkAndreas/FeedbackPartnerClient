42ca98aa947bb64fc8417d54105fcefa394fada6
Andreas Link <mail@linkandreas.de>
2025-01-19 20:37:08 +0100

    Configure output for export

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
   basePath: '/FeedbackPartnerClient',
   assetPrefix: '/FeedbackPartnerClient',
-};
-
-export default nextConfig;
+  distDir: 'dist',
+}
+ 
+module.exports = nextConfig
